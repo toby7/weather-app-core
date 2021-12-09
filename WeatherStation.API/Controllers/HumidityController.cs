295ae313191d98eb82a27bb3042c8aa21e007238
Tobias Olofsson <Tobias.Olofsson@sosalarm.se>
2021-12-09 19:55:18 +0100

    Clean up

--- WeatherStation.API/Controllers/HumidityController.cs
+++ WeatherStation.API/Controllers/HumidityController.cs
@@ -23,14 +23,14 @@ namespace WeatherStation.API.Controllers
             var netamoApi = RestService.For<INetamoApi>("https://api.netatmo.com/api/", new RefitSettings
             {
                 AuthorizationHeaderValueGetter = () => {
-                    var token = "61ab561fbc9b7a5a63743d60|b80e1ce48ddd3d85fcf797fd2b183ac4";
+                    var token = "";
                     return Task.FromResult(token);
                 }
             });
 
             try
             {
-                var test = await netamoApi.GetMeasure("70:ee:50:84:23:24");
+                var test = await netamoApi.GetMeasure("");
 
                 return this.Ok(test);
             }
